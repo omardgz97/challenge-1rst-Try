fe666eb802c21c4a59933e73d22da7bff5b659de
omardgz97 <omardgz97@gmail.com>
2024-07-23 00:07:02 -0600

    1er commit en pruebasScript, cambié las primeras variables let por const e introduje la función correcta para modificar la imagen del outputText

--- app.js
+++ app.js
@@ -1,58 +1,57 @@
-//Manejo del DOM//
+//-------------------Manejo del DOM----------------------------//
 
-let inputText = document.getElementById('inputText');
-let botonEncriptar = document.getElementById('encriptar');
-let botonDesencriptar = document.getElementById('desencriptar');
-let outputText = document.getElementById('outputText');
-let botonCopiar = document.getElementById('copiar');
-let imagenOutputText = document.getElementById('imagen__outputText')
+const inputText = document.getElementById('inputText');
+const botonEncriptar = document.getElementById('encriptar');
+const botonDesencriptar = document.getElementById('desencriptar');
+const outputText = document.getElementById('outputText');
+const botonCopiar = document.getElementById('copiar');
+const imagenOutputText = document.getElementById('imagen__outputText')
 
+//----------------------Eventos!-------------------------------//
+
+/* Eventos para desencriptar y encriptar */
 botonEncriptar.onclick = function (){
     let textoEncriptado = encriptar(inputText.value);
     console.log(`el mensaje encriptado es: "${textoEncriptado}"`);
     outputText.value = textoEncriptado;
+    mostrarOcultarImagen();
 };
 
 botonDesencriptar.onclick = function (){
     let textoDesencriptado = desencriptar(inputText.value);
     console.log(`el mensaje desencriptado es: ${textoDesencriptado}`);
     outputText.value = textoDesencriptado;
+    mostrarOcultarImagen();
 }
 
+/* Eventos para gestionar el Input */
 inputText.addEventListener ('input', function(){
     if(this.value === ""){
         outputText.value = "";
     }
+    mostrarOcultarImagen();
 });
 
 outputText.addEventListener ('input', function(){
-    if (this.value === ""){
-        inputText.value = "";
-    }
+    mostrarOcultarImagen();
 });
 
 botonCopiar.onclick = function (){
     let textoCopiado = outputText.value;
     navigator.clipboard.writeText(textoCopiado);
 }
-    
 
-
-/*
-function imgApareceDesaparece() {
+//---------función para la imagen del outputText!-------//
+  
+function mostrarOcultarImagen () {
     if (outputText.value === "") {
-        outputText.style.display = "none";
         imagenOutputText.style.display = "block";
     } else {
-        outputText.style.display = "block";
         imagenOutputText.style.display = "none";
     }
-}
-*/
+};
 
-
-
-//--------------función para encriptar!---------------//
+//--------------función para encriptar!-----------------//
 
 function encriptar (textoUsuario){
     let arrayParaEncriptar = textoUsuario.split("");
@@ -79,7 +78,7 @@ function encriptar (textoUsuario){
     return arrayParaEncriptar.join("");
 }
 
-//-----------Función para Desencriptar-----------//
+//--------------función para Desencriptar!--------------//
 
 function desencriptar(textoEncriptado) {
     let textoDesencriptado = textoEncriptado.split("ufat").join("u");
@@ -89,4 +88,4 @@ function desencriptar(textoEncriptado) {
     textoDesencriptado = textoDesencriptado.split("ai").join("a");
     
     return textoDesencriptado;
-}
+};
